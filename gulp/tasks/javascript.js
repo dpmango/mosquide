284364a2381e43c219b3c208f7ef604af23b9eb7
dpmango <xs290@me.com>
2017-09-05 00:25:10 +0300

    fixed bugs

--- gulp/tasks/javascript.js
+++ gulp/tasks/javascript.js
@@ -10,7 +10,6 @@ gulp.task('javascript:vendor', function() {
   return gulp.src([
       path.resolve('bower_components', 'jquery/dist/jquery.min.js'),
       path.resolve('bower_components', 'slick-carousel/slick/slick.min.js'),
-      path.resolve('bower_components', 'jquery-mask-plugin/dist/jquery.mask.min.js'),
       config.src.js + '/vendor/**/*.js'
      ])
     .pipe(config.production ? uglifyJs() : util.noop())
